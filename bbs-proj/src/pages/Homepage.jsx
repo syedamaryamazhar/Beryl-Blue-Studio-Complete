c42c58e9fba1c49aef04f0ca8b13c56028b62364
Syeda Maryam Azhar <syedamaryamazhar@gmail.com>
2026-01-05 09:03:09 +0500

    some fixes

--- bbs-proj/src/pages/Homepage.jsx
+++ bbs-proj/src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from "react";
-import {useNavigate, BrowserRouter, Routes, Route}  from 'react-router-dom';
+import {useNavigate}  from 'react-router-dom';
 import ProductCard from "/src/components/product-card";
 import './Homepage.css'
 
